f00bf137445aaa8172c80e5f5dd55b951b2ab296
DuStein <steindu@gmail.com>
2022-03-11 10:13:05 -0300

    config jest mas pra ver depois

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,18 @@
-// const pegaArquivo = require('../index');
+//não resolvido problema de compatibilidado com o jest, vou continuar pra não ficar preso nesse problema muito tempo mais
 import { pegaArquivo } from '../index.js';
 
-test('seria uma funcao', () => {
-    expect(typeof pegaArquivo).toBe('function');
-}
-)
+const arrayesult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
+
+describe('pegaArquivo :: ', () => {
+    it('deve ser uma função :', () => {
+        expect(typeof pegaArquivo).toBe('function');
+    })
+    it('deve retornar array com resultados', async () => {
+        const resultado = await pegaArquivo(C:\Users\DuStein\Documents\alura_nodejs_1\test\arquivos\file1.md)
+        expect(resultado).toEqual(arrayesult)
+    })
+})
